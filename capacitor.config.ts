5dd06d7eea369bd6d59e89606d7599ba81d8fc87
Jair Javier <jairtareas@outlook.com>
2026-05-27 19:24:15 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'import-supplies-protection',
+  webDir: 'www'
+};
+
+export default config;
